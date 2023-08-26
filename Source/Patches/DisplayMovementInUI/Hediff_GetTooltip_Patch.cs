8b3eacb3ea423458775c710c7e59fc4f692a9407
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-08-26 22:57:06 +0200

    Show granted locomotion type in hediff tooltips.

---
+++ Source/Patches/DisplayMovementInUI/Hediff_GetTooltip_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace PathfindingFramework.Patches.DisplayMovementInUI
+{
+	[HarmonyPatch(typeof(Hediff), nameof(Hediff.GetTooltip))]
+	internal static class Hediff_GetTooltip_Patch
+	{
+		internal static void Postfix(Hediff __instance, ref string __result)
+		{
+			var extension = __instance.def.GetModExtension<MovementExtension>();
+			if (extension != null)
+			{
+				__result +=
+					$"\n\n{"PF_GrantsLocomotion".Translate().CapitalizeFirst().Colorize(ColoredText.TipSectionTitleColor)}\n  - {extension.movementDef.LabelCap}";
+			}
+		}
+	}
+}
