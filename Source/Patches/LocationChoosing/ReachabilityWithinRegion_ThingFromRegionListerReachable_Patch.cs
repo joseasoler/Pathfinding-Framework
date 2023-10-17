11f6fcb4fc33adf0240c07e1fb38bf85dc75ea68
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-10-17 19:06:05 +0200

    Patch vanilla code for reachable things in a region. Resolves #57. Resolves #58.

---
+++ Source/Patches/LocationChoosing/ReachabilityWithinRegion_ThingFromRegionListerReachable_Patch.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using HarmonyLib;
+using PathfindingFramework.MovementContexts;
+using Verse;
+using Verse.AI;
+
+namespace PathfindingFramework.Patches.LocationChoosing
+{
+	/// <summary>
+	/// Pawns can only reach things in cells they can stand over.
+	/// </summary>
+	[HarmonyPatch(typeof(ReachabilityWithinRegion), nameof(ReachabilityWithinRegion.ThingFromRegionListerReachable))]
+	internal static class ReachabilityWithinRegion_ThingFromRegionListerReachable_Patch
+	{
+		private static void Postfix(ref bool __result, Thing thing,
+			Region region,
+			PathEndMode peMode,
+			Pawn traveler)
+		{
+			if (!__result)
+			{
+				return;
+			}
+
+			Map map = region.Map;
+			if (peMode == PathEndMode.ClosestTouch)
+			{
+				peMode = GenPath.ResolveClosestTouchPathMode(traveler, map, thing.Position);
+			}
+
+			MovementContext context = traveler.MovementContext();
+			switch (peMode)
+			{
+				case PathEndMode.OnCell:
+				case PathEndMode.Touch:
+					if (thing.def.size.x == 1 && thing.def.size.z == 1)
+					{
+						if (!context.CanStandAt(thing.Position))
+						{
+							__result = false;
+						}
+					}
+					else
+					{
+						bool canStandAtAnyCell = false;
+						foreach (IntVec3 loc in thing.OccupiedRect())
+						{
+							if (context.CanStandAt(thing.Position))
+							{
+								canStandAtAnyCell = true;
+								break;
+							}
+						}
+
+						__result = canStandAtAnyCell;
+					}
+
+					break;
+				case PathEndMode.InteractionCell:
+					if (!context.CanStandAt(thing.InteractionCell))
+					{
+						__result = false;
+					}
+
+					break;
+				case PathEndMode.None:
+				case PathEndMode.ClosestTouch:
+				default:
+					break;
+			}
+		}
+	}
+}
