6ca3791df9439be3e28f1df22cd1f424734131af
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-10-21 07:46:54 +0200

    Consider regions with vanilla impassable terrain as enclosed for pens. Resolves #59.

---
+++ Source/Patches/LocationChoosing/AnimalPenEnclosureCalculator_EnterRegion_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace PathfindingFramework.Patches.LocationChoosing
+{
+	/// <summary>
+	/// Consider regions with vanilla impassable terrains as enclosed.
+	/// </summary>
+	[HarmonyPatch(typeof(AnimalPenEnclosureCalculator), "EnterRegion")]
+	internal static class AnimalPenEnclosureCalculator_EnterRegion_Patch
+	{
+		private static void Postfix(ref bool __result, Region from, Region to)
+		{
+			__result &= to.TerrainDef() == null;
+		}
+	}
+}
