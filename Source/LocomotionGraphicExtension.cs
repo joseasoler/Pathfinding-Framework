1a95af1041b1b3c6a5b5589db5d75c8b9326eef6
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-10-24 21:52:00 +0200

    Digging movement type.

--- Source/LocomotionGraphicExtension.cs
+++ Source/LocomotionGraphicExtension.cs
@@ -7,7 +7,7 @@ namespace PathfindingFramework
 	/// <summary>
 	/// Applies a graphic change when the pawn is moving at one of the listed locomotion types.
 	/// Must be added to the PawnKindDef of the pawn. Incompatible with human-like pawns.
-	/// Not compatible with PawnKindDefs which have different graphics on their life stages.
+	/// This extension assumes that the PawnKindDef has the same graphics for all of its life stages.
 	/// Changes from this extension take precedence over changes from TerrainTagGraphicExtension.
 	/// </summary>
 	public class LocomotionGraphicExtension : GraphicExtension
