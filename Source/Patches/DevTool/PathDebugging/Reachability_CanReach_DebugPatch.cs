53f8e95e304875d0f7682ffb3a9f7451d865b337
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-10-30 07:12:05 +0100

    Add in-bounds checks to the reachability debug code. Resolves #76.

--- Source/Patches/DevTool/PathDebugging/Reachability_CanReach_DebugPatch.cs
+++ Source/Patches/DevTool/PathDebugging/Reachability_CanReach_DebugPatch.cs
@@ -10,7 +10,7 @@ namespace PathfindingFramework.Patches.DevTool.PathDebugging
 		typeof(PathEndMode), typeof(TraverseParms))]
 	internal static class Reachability_CanReach_DebugPatch
 	{
-		// Code for debugging reachability issues.
+		// Code for debugging reachability issues. WARNING: VERY spammy.
 		private static void Postfix(bool __result, IntVec3 start, LocalTargetInfo dest, PathEndMode peMode,
 			TraverseParms traverseParams)
 		{
@@ -19,9 +19,9 @@ namespace PathfindingFramework.Patches.DevTool.PathDebugging
 			Map map = pawn != null ? pawn.Map : thing?.Map;
 			string pawnStr = pawn != null ? pawn.GetUniqueLoadID() : "None";
 			string startStr = $"[{start.x}, {start.z}]";
-			string startTerrain = map != null ? start.GetTerrain(map).defName : "Unknown";
+			string startTerrain = map != null && start.InBounds(map) ? start.GetTerrain(map).defName : "Unknown";
 			string destStr = $"[{dest.Cell.x}, {dest.Cell.z}]";
-			string destTerrain = map != null ? dest.Cell.GetTerrain(map).defName : "Unknown";
+			string destTerrain = map != null && dest.Cell.InBounds(map) ? dest.Cell.GetTerrain(map).defName : "Unknown";
 			string targetStr = thing != null ? thing.GetUniqueLoadID() : "Cell";
 			string traverseModeStr = Enum.GetName(typeof(TraverseMode), traverseParams.mode);
 			string pathEndMode = Enum.GetName(typeof(PathEndMode), peMode);
@@ -30,5 +30,5 @@ namespace PathfindingFramework.Patches.DevTool.PathDebugging
 				$"{pawnStr} requests a Reachability.CanReach from {startStr}[{startTerrain}] to {destStr}[{destTerrain}] to reach {targetStr}. Using traverse mode {traverseModeStr} and path end mode {pathEndMode}. Result is {__result}");
 		}
 	}
-			*/
+	*/
 }
