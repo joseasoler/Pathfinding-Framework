655ccd74c5055ec3bd52c79c0f173a98d4366189
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-10-31 21:46:25 +0100

    Set cells as dirty when their walkability changes for any movement type.

--- Source/Patches/TerrainChanges/TerrainGrid_DoTerrainChangedEffects_Patch.cs
+++ Source/Patches/TerrainChanges/TerrainGrid_DoTerrainChangedEffects_Patch.cs
@@ -5,13 +5,29 @@ namespace PathfindingFramework.Patches.TerrainChanges
 {
 	/// <summary>
 	/// Handle updates to the terrain path cost grids.
+	/// Trigger region updates when necessary.
 	/// </summary>
 	[HarmonyPatch(typeof(TerrainGrid), "DoTerrainChangedEffects")]
 	internal static class TerrainGrid_DoTerrainChangedEffects_Patch
 	{
 		internal static void Postfix(Map ___map, IntVec3 c)
 		{
-			___map?.MovementContextData()?.UpdateCell(c);
+			if (___map == null)
+			{
+				return;
+			}
+
+			___map.MovementContextData()?.UpdateCell(c);
+
+			// PathGrid.RecalculatePerceivedPathCostAt uses the vanilla path grid to determine when the regions
+			// should be updated. These extra checks take care of PF specific cases.
+			TerrainDef terrainDef = c.GetTerrain(___map);
+			TerrainDef regionTerrainDef = ___map.regionGrid.GetRegionAt_NoRebuild_InvalidAllowed(c)?.UniqueTerrainDef();
+			int extendedRegionType = regionTerrainDef?.ExtendedRegionType() ?? 0;
+			if (extendedRegionType != terrainDef.ExtendedRegionType())
+			{
+				___map.regionDirtyer.Notify_WalkabilityChanged(c, true);
+			}
 		}
 	}
 }
