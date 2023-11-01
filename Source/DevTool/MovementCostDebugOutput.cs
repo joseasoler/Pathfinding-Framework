475a7a3491f42545ff99af18bce8dc82d3ec361f
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-11-01 19:34:28 +0100

    Improve movement cost debug output.

--- Source/DevTool/MovementCostDebugOutput.cs
+++ Source/DevTool/MovementCostDebugOutput.cs
@@ -17,19 +17,22 @@ namespace PathfindingFramework.DevTool
 			int movementCount = movementDefs.Count;
 			int terrainCount = terrainDefs.Count;
 
-			var dataTable = new string[movementCount + 1, terrainCount + 1];
-			dataTable[0, 0] = "Locomotion type";
+			var dataTable = new string[movementCount + 2, // Terrain, terrainindex, ...
+				terrainCount + 1];
+			dataTable[0, 0] = "TerrainDef";
+			dataTable[1, 0] = "Terrain Index";
 
 			for (var movementIndex = 0; movementIndex < movementCount; ++movementIndex)
 			{
 				MovementDef movementDef = movementDefs[movementIndex];
-				dataTable[movementIndex + 1, 0] = movementDef.label;
+				dataTable[movementIndex + 2, 0] = movementDef.label;
 
 				for (var terrainIndex = 0; terrainIndex < terrainCount; ++terrainIndex)
 				{
 					TerrainDef terrainDef = terrainDefs[terrainIndex];
-					dataTable[0, terrainIndex + 1] = terrainDef.label;
-					dataTable[movementIndex + 1, terrainIndex + 1] = new PathCost(movementDef.PathCosts[terrainIndex]).ToString();
+					dataTable[0, terrainIndex + 1] = terrainDef.defName;
+					dataTable[1, terrainIndex + 1] = terrainDef.index.ToString();
+					dataTable[movementIndex + 2, terrainIndex + 1] = new PathCost(movementDef.PathCosts[terrainIndex]).ToString();
 				}
 			}
 
