53b9f1c39bd124f2d54a91b7cdb643262c4c387e
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-11-12 11:44:20 +0100

    Encapsulate reset button code.

--- Source/SettingsWindow.cs
+++ Source/SettingsWindow.cs
@@ -112,6 +112,26 @@ namespace PathfindingFramework
 		}
 
 		/// <summary>
+		/// Draw additional buttons on the bottom button bar of the mod settings window.
+		/// </summary>
+		/// <param name="inRect">Available area for drawing the settings.</param>
+		private static void DrawBottomButtons(Rect inRect)
+		{
+			float resetX = inRect.width - Window.CloseButSize.x;
+			// Dialog_ModSettings leaves a margin of Window.CloseButSize.y at the bottom for the close button.
+			// Then, there are three pixels between the top border of the close button and the rest of this window.
+			float resetY = inRect.height + Window.CloseButSize.y + 3;
+			Rect resetButtonArea = new Rect(resetX, resetY, Window.CloseButSize.x, Window.CloseButSize.y);
+
+			if (Widgets.ButtonText(resetButtonArea, "PF_ResetSettingsLabel".Translate()))
+			{
+				Settings.Reset();
+			}
+
+			TooltipHandler.TipRegion(resetButtonArea, "PF_ResetSettingsHover".Translate());
+		}
+
+		/// <summary>
 		/// Contents of the mod settings window.
 		/// </summary>
 		/// <param name="inRect">Available area for drawing the settings.</param>
@@ -125,21 +145,7 @@ namespace PathfindingFramework
 
 			DoTabContents(settingsArea.ContractedBy(15.0f));
 
-			// Draw the reset button at the right side of the area that Dialog_ModSettings reserves for the close button.
-
-			// The reset button is placed at the bottom right corner.
-			float resetX = inRect.width - Window.CloseButSize.x;
-			// Dialog_ModSettings leaves a margin of Window.CloseButSize.y at the bottom for the close button.
-			// Then, there are three pixels between the top border of the close button and the rest of this window.
-			float resetY = inRect.height + Window.CloseButSize.y + 3;
-			Rect resetButtonArea = new Rect(resetX, resetY, Window.CloseButSize.x, Window.CloseButSize.y);
-
-			if (Widgets.ButtonText(resetButtonArea, "PF_ResetSettingsLabel".Translate()))
-			{
-				Settings.Reset();
-			}
-
-			TooltipHandler.TipRegion(resetButtonArea, "PF_ResetSettingsHover".Translate());
+			DrawBottomButtons(inRect);
 		}
 
 		public static void OnWriteSettings()
