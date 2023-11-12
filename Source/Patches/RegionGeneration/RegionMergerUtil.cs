578b6f6ae1447eacea83b3e913d69e5225c787da
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-11-12 23:57:51 +0100

    Compatibility patch for Dubs Bad Hygiene.

--- Source/Patches/RegionGeneration/RegionMergerUtil.cs
+++ Source/Patches/RegionGeneration/RegionMergerUtil.cs
@@ -10,10 +10,12 @@ namespace PathfindingFramework.Patches.RegionGeneration
 	{
 		private static RegionEntryPredicate GeneratePredicate(RegionEntryPredicate originalPredicate)
 		{
+			const RegionType combinableRegion = RegionType.Fence | RegionType.Normal;
+
 			// For vanilla-like regions, their TerrainDef call will return null.
 			// For regions composed of terrains that must have unique regions, this returns their terrain.
 			return (from, to) => originalPredicate(from, to) &&
-				(to.type != RegionType.Normal || from.UniqueTerrainDef() == to.UniqueTerrainDef());
+				((to.type & combinableRegion) == 0 || from.UniqueTerrainDef() == to.UniqueTerrainDef());
 		}
 
 		public static IEnumerable<CodeInstruction> PatchRegionMergingPredicate(IEnumerable<CodeInstruction> instructions)
