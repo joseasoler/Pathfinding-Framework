fd6075af62938b198384691f7bf07106f706c0ad
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-11-12 21:53:27 +0100

    Work in progress implementation of the new pawn movement tab. Related to #98.

--- Source/SettingsWindow.cs
+++ Source/SettingsWindow.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using PathfindingFramework.Patches;
+using RimWorld;
 using UnityEngine;
 using Verse;
 
@@ -12,7 +14,37 @@ namespace PathfindingFramework
 	public enum SettingsWindowTab
 	{
 		General,
+		PawnMovement,
 		Debugging,
+	}
+
+	/// <summary>
+	/// Sorts entries of the pawn movement table. Assumes that all entries are non-null and have a race.
+	/// </summary>
+	public class ComparePawnMovementEntries : IComparer<Pair<ThingDef, Texture>>
+	{
+		public int Compare(Pair<ThingDef, Texture> lhs, Pair<ThingDef, Texture> rhs)
+		{
+			bool humanlikeLhs = lhs.First.race.Humanlike;
+			bool humanlikeRhs = rhs.First.race.Humanlike;
+
+			int humanlikeCompare = humanlikeLhs.CompareTo(humanlikeRhs);
+			if (humanlikeCompare != 0)
+			{
+				return -humanlikeCompare;
+			}
+
+			bool animalLhs = lhs.First.race.Animal;
+			bool animalRhs = rhs.First.race.Animal;
+
+			int animalCompare = animalLhs.CompareTo(animalRhs);
+			if (animalCompare != 0)
+			{
+				return -animalCompare;
+			}
+
+			return string.Compare(lhs.First.label, rhs.First.label, StringComparison.OrdinalIgnoreCase);
+		}
 	}
 
 	/// <summary>
@@ -39,6 +71,71 @@ namespace PathfindingFramework
 		/// Used to trigger a write settings call when tabs change.
 		/// </summary>
 		private static Action _writeSettingsAction;
+
+		/// <summary>
+		/// Current scroll position of the PawnMovement tab.
+		/// </summary>
+		// private static int _firstPawnMovementIndex = 0;
+
+		/// <summary>
+		/// List of entries of the pawn movement window.
+		/// </summary>
+		private static List<Pair<ThingDef, Texture>> _pawnMovementEntries;
+
+		private static Texture GetTextureFromPawnKindDef(PawnKindDef pawnKindDef)
+		{
+			if (pawnKindDef.lifeStages == null)
+			{
+				return null;
+			}
+
+			int lastLifeStageIndex = pawnKindDef.lifeStages.Count - 1;
+			if (lastLifeStageIndex < 0)
+			{
+				return null;
+			}
+
+			PawnKindLifeStage lastLifeStage = pawnKindDef.lifeStages[lastLifeStageIndex];
+			return lastLifeStage.bodyGraphicData?.Graphic?.MatSingle.mainTexture;
+		}
+
+		private static bool IsValidPawnKindDef(PawnKindDef pawnKindDef, HashSet<ThingDef> ignoreDuplicates)
+		{
+			// Avoid showing Vehicles Framework pawns.
+			const string vehiclesFrameworkPawnKindDef = "VehicleDef";
+
+			return pawnKindDef.race?.race != null &&
+				pawnKindDef.race.GetType().Name != vehiclesFrameworkPawnKindDef &&
+				!ignoreDuplicates.Contains(pawnKindDef.race);
+		}
+
+		private static List<Pair<ThingDef, Texture>> GetPawnMovementEntries()
+		{
+			if (_pawnMovementEntries != null)
+			{
+				return _pawnMovementEntries;
+			}
+
+			_pawnMovementEntries = new List<Pair<ThingDef, Texture>>();
+			HashSet<ThingDef> ignoreDuplicates = new HashSet<ThingDef>();
+
+			List<PawnKindDef> pawnKindDefs = DefDatabase<PawnKindDef>.AllDefsListForReading;
+			for (int pawnIndex = 0; pawnIndex < pawnKindDefs.Count; ++pawnIndex)
+			{
+				PawnKindDef pawnKindDef = pawnKindDefs[pawnIndex];
+				if (!IsValidPawnKindDef(pawnKindDef, ignoreDuplicates))
+				{
+					continue;
+				}
+
+				ThingDef raceThingDef = pawnKindDef.race;
+				ignoreDuplicates.Add(raceThingDef);
+				_pawnMovementEntries.Add(new Pair<ThingDef, Texture>(raceThingDef, GetTextureFromPawnKindDef(pawnKindDef)));
+			}
+
+			_pawnMovementEntries.Sort(new ComparePawnMovementEntries());
+			return _pawnMovementEntries;
+		}
 
 		/// <summary>
 		/// Initialize the settings window. Should be called only once before it is shown.
@@ -75,40 +172,62 @@ namespace PathfindingFramework
 			return tabs;
 		}
 
-		private static void DoGeneralTabContents(Listing_Standard listing)
+		private static void DoGeneralTabContents(Rect inRect)
 		{
+			Listing_Standard listing = new Listing_Standard();
+			listing.Begin(inRect);
+
 			listing.CheckboxLabeled("PF_IgnoreFireLabel".Translate(), ref Settings.Values.IgnoreFire,
 				"PF_IgnoreFireHover".Translate());
+
+			listing.End();
 		}
 
-		private static void DoDebuggingTabContents(Listing_Standard listing)
+		private static void DoPawnMovementTabContents(Rect inRect)
 		{
+			// const float animalEntryHeight = 64F;
+
+			Listing_Standard listing = new Listing_Standard();
+
+			// ToDo leave space for the vertical scroll bar.
+			listing.Begin(inRect);
+
+			// GUI.VerticalScrollbar(new Rect(screenRect.xMax + (float) verticalScrollbar.margin.left, screenRect.y, verticalScrollbar.fixedWidth, screenRect.height), scrollPosition.y, Mathf.Min(screenRect.height, viewRect.height), 0.0f, viewRect.height, verticalScrollbar);
+
+			listing.End();
+		}
+
+		private static void DoDebuggingTabContents(Rect inRect)
+		{
+			Listing_Standard listing = new Listing_Standard();
+			listing.Begin(inRect);
+
 			listing.CheckboxLabeled("PF_InspectorLabel".Translate(), ref Settings.Values.Inspectors,
 				"PF_InspectorHover".Translate());
 			listing.CheckboxLabeled("PF_LogPathNotFoundLabel".Translate(), ref Settings.Values.LogPathNotFound,
 				"PF_LogPathNotFoundHover".Translate());
 			listing.CheckboxLabeled("PF_DebugLogLabel".Translate(), ref Settings.Values.DebugLog,
 				"PF_DebugLogHover".Translate());
+
+			listing.End();
 		}
 
 		private static void DoTabContents(Rect inRect)
 		{
-			Listing_Standard listing = new Listing_Standard();
-			listing.Begin(inRect);
-
 			switch (_tab)
 			{
 				case SettingsWindowTab.General:
-					DoGeneralTabContents(listing);
+					DoGeneralTabContents(inRect);
+					break;
+				case SettingsWindowTab.PawnMovement:
+					DoPawnMovementTabContents(inRect);
 					break;
 				case SettingsWindowTab.Debugging:
-					DoDebuggingTabContents(listing);
+					DoDebuggingTabContents(inRect);
 					break;
 				default:
 					throw new ArgumentOutOfRangeException();
 			}
-
-			listing.End();
 		}
 
 		/// <summary>
