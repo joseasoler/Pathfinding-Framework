6750bcb000160122d27bb802799847c413ea836f
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-01-06 12:08:50 +0100

    Update pawn movement type when the pawn drops apparel. Resolves #114.

---
+++ Source/Patches/MovementMayHaveChanged/Pawn_ApparelTracker_TryDrop_Patch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using PathfindingFramework.PawnMovement;
+using RimWorld;
+using Verse;
+
+namespace PathfindingFramework.Patches.MovementMayHaveChanged
+{
+	/// <summary>
+	/// Update the pawn movement cache when an apparel with a movement extension is dropped.
+	/// </summary>
+	[HarmonyPatch(typeof(Pawn_ApparelTracker), nameof(Pawn_ApparelTracker.TryDrop),
+		new[] {typeof(Apparel), typeof(Apparel), typeof(IntVec3), typeof(bool)},
+		new[] {ArgumentType.Normal, ArgumentType.Out, ArgumentType.Normal, ArgumentType.Normal})]
+	public static class Pawn_ApparelTracker_TryDrop_Patch
+	{
+		public static void Postfix(Pawn ___pawn, Apparel ap)
+		{
+			if (___pawn.Spawned && ap.def.MovementDef() != null)
+			{
+				PawnMovementUpdater.Update(___pawn);
+			}
+		}
+	}
+}
