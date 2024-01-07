c8d151b206c34ce2c145941c0234feb1f7b82fbc
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-01-07 14:29:53 +0100

    Remove year from copyright disclaimers.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Pathfinding Framework")]
-[assembly: AssemblyCopyright("Copyright © 2023 Pathfinding Framework contributors")]
+[assembly: AssemblyCopyright("Copyright © Pathfinding Framework contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
